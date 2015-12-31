8165edb0fc9eaf85d8c50c63111eb7ae9756ec0f
FRANCIS HUYNH <fhools@yeezy.local>
2015-12-30 23:15:37 -0800

    Finished Chapter 1
    Finished chapter 1, except did not set an app icon.

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by FRANCIS HUYNH on 12/30/15.
+//  Copyright © 2015 Fhools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?",
+                                "What is 7+7",
+                                "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+        
+    var currentQuestionIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+
+
+    @IBAction func showNextAnswer(sender: AnyObject) {
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
+
