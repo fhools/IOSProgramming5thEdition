3aa0c6f015bd5be98b2120427f0c732d979981f6
FRANCIS HUYNH <fhools@yeezy.local>
2016-01-01 22:26:53 -0800

    Chapter 3 - Part 2 Set initial widget layouts using auto layout
    Set initial ui using auto layout

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -10,19 +10,5 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let frame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let subview = UIView(frame: frame)
-        subview.backgroundColor = UIColor.blueColor()
-        view.addSubview(subview)
-        
-        let subview2 = UIView(frame: CGRect(x: 20, y: 30, width: 50, height: 50))
-        subview2.backgroundColor = UIColor.greenColor()
-        subview.addSubview(subview2)
-    }
-
-   
-
 }
 
