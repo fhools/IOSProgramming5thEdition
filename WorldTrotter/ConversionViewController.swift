40cb7c99895b39b73d07421062ea8eadebe525ee
FRANCIS HUYNH <fhools@yeezy.local>
2016-01-03 22:59:43 -0800

    Chapter 4: Part 2 Implement temperature conversion
    Implement actual temperature conversion after user imports.
    Uses:
    Computed property to do celsius conversion
    Property observer to update view when model changes
    Number formatter to format Double decimal precision
    UITextField delegate to disallow invalid user input with multiple ‘.’
    entered for decimal value.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,19 +8,60 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5.0/9.0)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter =  {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
+        if let text = textField.text, value = Double(text){
+            fahrenheitValue = value
         } else {
-            celsiusLabel.text = "???"
+            fahrenheitValue = nil
         }
     }
     
     @IBAction func dismissKeyboard(sender: AnyObject) {
         textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    //MARK: UITextField Delegates
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+        
     }
 }
