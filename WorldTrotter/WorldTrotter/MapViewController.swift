72a36197ae623a1d1917a9d287af63d520a55d81
FRANCIS HUYNH <fhools@yeezy.local>
2016-01-07 22:04:26 -0800

    Chapter 6 - Part 1. Review Map View from Interface builder
    Create MapView programmatically in loadView method of MapViewCotroller

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import MapKit
 class MapViewController : UIViewController {
+    
+    var mapView: MKMapView!
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view")
     }
+    
 }
