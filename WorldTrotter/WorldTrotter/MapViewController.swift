b9ae093bec6c2d1ebbf88b441486633906aa9b0e
FRANCIS HUYNH <fhools@yeezy.local>
2016-01-08 22:22:27 -0800

    Chapter 6 Finished Gold Challenge
    Finish Chapter 6
    Added 3 pins to the map
    Used CLLocationCoordinate2D to enter in long/lat coordinates
    Used MKPoiintAnnotation  to add coordinates to pins
    Used MkMapPointForCoordinate to zoom into selection
    mapView.setVisiblePoint is what zooms in.

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -12,9 +12,14 @@ class MapViewController : UIViewController {
     
     var mapView: MKMapView!
     let cl = CLLocationManager()
+    var pins = [MKPointAnnotation]()
+    var pinIndex = 0
     override func loadView() {
         mapView = MKMapView()
         view = mapView
+        
+        // Ensure we have access to Location Services
+        cl.requestAlwaysAuthorization()
         
         // Create segment control
         let segmentControl = UISegmentedControl(items: [ "Standard", "Hybrid", "Satellite"])
@@ -39,7 +44,7 @@ class MapViewController : UIViewController {
         
         // Add zoom in and out button
         let findButton = UIButton()
-        findButton.setTitle("✸", forState: .Normal)
+        findButton.setTitle("Find Me", forState: .Normal)
         findButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
         findButton.frame = CGRectMake(15, -50, 300, 500)
         findButton.titleLabel!.text = "Find"
@@ -48,9 +53,45 @@ class MapViewController : UIViewController {
         view.addConstraint(xCenterConstraint)
         findButton.translatesAutoresizingMaskIntoConstraints = false
         findButton.addTarget(self, action: "findMe:", forControlEvents: .TouchUpInside)
-        let bottomConstraint = findButton.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor, constant: -20)
+        let findbottomConstraint = findButton.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor, constant: -20)
        
-        bottomConstraint.active = true
+        findbottomConstraint.active = true
+
+        let cycleButton = UIButton()
+        cycleButton.setTitle("CyclePins", forState: .Normal)
+        cycleButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        cycleButton.frame = CGRectMake(40, -100, 300, 500)
+        cycleButton.titleLabel!.text = "CyclePins"
+        view.addSubview(cycleButton)
+        let x2CenterConstraint = NSLayoutConstraint(item: view, attribute: .CenterX, relatedBy: .Equal, toItem: cycleButton, attribute: .CenterX, multiplier: 1, constant: 0)
+        view.addConstraint(x2CenterConstraint)
+        cycleButton.translatesAutoresizingMaskIntoConstraints = false
+        cycleButton.addTarget(self, action: "cyclePins:", forControlEvents: .TouchUpInside)
+        let cyclebottomConstraint = cycleButton.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor, constant: -50)
+        
+        cyclebottomConstraint.active = true
+
+        
+        // Gold Challenge: Add 3 pins on the map
+       
+        let pin1 = MKPointAnnotation()
+        pin1.coordinate =  CLLocationCoordinate2DMake(37.3882601, -122.0302504)
+        pin1.title = "Pizza Hut"
+ //       MKMapPointForCoordinate(<#T##coordinate: CLLocationCoordinate2D##CLLocationCoordinate2D#>)
+        
+        let pin2 = MKPointAnnotation()
+        pin2.coordinate = CLLocationCoordinate2DMake(37.3854416,-122.0421551)
+        pin2.title = "Home"
+        let pin3 = MKPointAnnotation()
+        pin3.coordinate = CLLocationCoordinate2DMake(40.6892534,-74.0466944)
+        pin3.title = "StatueOfLiberty"
+        pins = [pin1, pin2, pin3]
+        for p in pins {
+                mapView.addAnnotation(p)
+        }
+        
+        
+        
         
         
     }
@@ -77,9 +118,21 @@ class MapViewController : UIViewController {
     @IBAction func findMe(stepper: UIButton) {
         print("User pressed find me")
         
-        cl.requestAlwaysAuthorization()
+       
         mapView.setUserTrackingMode(.Follow,
             animated: true)
         mapView.showsUserLocation = true
+    }
+    
+    @IBAction func cyclePins(sender: UIButton) {
+        print("Cycling throug pins")
+        let point = MKMapPointForCoordinate(mapView.annotations[pinIndex].coordinate)
+        let pointRect = MKMapRectMake(point.x , point.y, 0.1, 0.1)
+        mapView.setVisibleMapRect(pointRect, animated: true)
+        pinIndex += 1
+        if pinIndex >= 3 {
+            pinIndex = 0
+        }
+        
     }
 }
